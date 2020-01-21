2f471dd8f10006687b06efa70115bc533d034b73
Nick Meldrum <nick@nickmeldrum.com>
2020-01-21 09:32:33 +0000

    Fix column dragging styles

--- clients/web/src/components/Column/Column.tsx
+++ clients/web/src/components/Column/Column.tsx
@@ -15,34 +15,40 @@ const useStyles = makeStyles(theme => ({
   column: {
     flex: 1,
     margin: '5px',
-    border: '1px solid rgba(0,0,0,0)'
+    border: '1px solid rgba(0,0,0,0)',
+    '& h2': {
+      fontSize: '20px',
+      padding: '10px',
+      borderBottom: '1px solid rgba(120,120,120,0.5)',
+      margin: '0px 5px 15px 5px',
+    },
   },
-  heading: {
-    fontSize: '20px',
-    padding: '10px',
-    borderBottom: '1px solid rgba(120,120,120,0.5)',
-    margin: '0px 5px 15px 5px'
+  highlight: {
+    border: '1px dashed rgb(255, 64, 129)',
+    '& h2': {
+      borderBottom: '1px dashed rgb(255, 64, 129)',
+    },
   },
 }));
 
 const Column: React.FC<ColumnProps> = ({column, stickies}) => {
-  const classes = useStyles();
   const updateStickyColumn = useStoreActions(actions => actions.data.boards.updateStickyColumn);
   const [{ isOver, canDrop }, drop] = useDrop({
     accept: Types.Sticky,
-    canDrop: (item: any) => item.currentColumn !== column,
-    drop: (item: any) => {
-      updateStickyColumn({ id: item.id, newColumn: column as string});
-    },
+    canDrop: (item: any) => item.column !== column,
+    drop: (item: any) => updateStickyColumn({ id: item.id, newColumn: column as string}),
     collect: monitor => ({
       isOver: !!monitor.isOver(),
       canDrop: !!monitor.canDrop(),
     }),
   });
+  const classes = useStyles();
+  let columnClass = classes.column;
+  if (isOver && canDrop) columnClass = `${columnClass} ${classes.highlight}`;
   return (
   <>
-  <div className={classes.column} ref={drop}>
-    <h2 className={classes.heading}>{column + isOver.toString()}</h2>
+  <div className={columnClass} ref={drop}>
+    <h2>{column}</h2>
     <ul>{stickies.map(sticky =>
       <Sticky
           key={sticky.id}
